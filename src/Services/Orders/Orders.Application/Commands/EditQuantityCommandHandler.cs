048827272c499d084ff1e7fd5955e43ecb539a27
tcerit <tunccerit@gmail.com>
2022-08-28 23:27:51 +0300

    Order item edit and addition

--- src/Services/Orders/Orders.Application/Commands/EditQuantityCommandHandler.cs
+++ src/Services/Orders/Orders.Application/Commands/EditQuantityCommandHandler.cs
@@ -7,27 +7,27 @@ using Orders.Domain;
 
 namespace Orders.Application.Commands
 {
-    public class AddOrderItemCommandHandler : IRequestHandler<AddOrderItemCommand, Unit>
+	public class EditQuantityCommandHandler : IRequestHandler<EditQuantityCommand, Unit>
     {
 
         private readonly IOrderRepository _repository;
         private readonly IRepository<OrderProduct> _prodcutRepository;
 
-        public AddOrderItemCommandHandler(IOrderRepository repository, IRepository<OrderProduct> prodcutRepository)
+        public EditQuantityCommandHandler(IOrderRepository repository, IRepository<OrderProduct> prodcutRepository)
         {
             _repository = repository;
             _prodcutRepository = prodcutRepository;
         }
 
 
-        public async Task<Unit> Handle(AddOrderItemCommand cmd, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(EditQuantityCommand cmd, CancellationToken cancellationToken)
         {
             Order? order = await _repository.GetByIdAsync(cmd.OrderId);
             Guard.Against.Null(order, nameof(Order), "Order not found");
             OrderProduct? product = await _prodcutRepository.GetByIdAsync(cmd.ProdcutId);
             Guard.Against.Null(product, nameof(OrderProduct), "Product not found");
 
-            order.AddItem(product, cmd.Quantity);
+            order.SetQuantity(product, cmd.Quantity);
 
             await _repository.UpdateAsync(order);
 
@@ -35,13 +35,13 @@ namespace Orders.Application.Commands
         }
     }
 
-    public class AddOrderItemCommand : IRequest<Unit>
+    public class EditQuantityCommand : IRequest<Unit>
     {
         public Guid OrderId { get; private set; }
         public Guid ProdcutId { get; internal set; }
         public int Quantity { get; internal set; }
 
-        public AddOrderItemCommand(Guid orderId, Guid prodcutId, int quantity)
+        public EditQuantityCommand(Guid orderId, Guid prodcutId, int quantity)
         {
             OrderId = orderId;
             ProdcutId = prodcutId;
@@ -49,6 +49,4 @@ namespace Orders.Application.Commands
         }
     }
 }
-
-
 

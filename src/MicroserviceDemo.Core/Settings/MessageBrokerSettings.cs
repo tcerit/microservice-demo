76d6ecc57e631704e17b3dd7d8d7f3b6fb66567d
Tunç Cerit <tunc@emodayazilim.com>
2022-09-03 16:22:43 +0300

    Update MessageBrokerSettings.cs
    Uri for message broker settings

--- src/MicroserviceDemo.Core/Settings/MessageBrokerSettings.cs
+++ src/MicroserviceDemo.Core/Settings/MessageBrokerSettings.cs
@@ -3,10 +3,7 @@ namespace Core.Settings
 {
     public class MessageBrokerSettings
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string VirtualHost { get; set; }
-        public string HostName { get; set; }
+        public string Uri { get; set; }
     }
 }
 
